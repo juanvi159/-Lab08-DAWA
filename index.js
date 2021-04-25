4edbcab7fd3ab17b2deffaf60c6e992b5d248894
VicenteObando <juan.obando.a@tecsup.edu.pe>
2021-04-25 06:25:09 -0500

    Lab06 1.- Listar personas

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const lista = [
+    {
+        id:1,
+        name: "Arto Hellias",
+        number: "040-123456",
+    },
+    {
+        id:2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+    },
+    {
+        id:3,
+        name: "Dan Abramov",
+        number: "12-43-234345",
+    },
+    {
+        id:4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122",
+    },
+]
+
+app.get('/api/persons',(req,res) => {
+    res.send(lista);
+});
+
+app.listen(3001,()=>console.log('Abriendo en el Puerto 3001'))
