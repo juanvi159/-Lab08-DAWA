7f813212da18fe3c723946234a4c8f6487529e22
VicenteObando <juan.obando.a@tecsup.edu.pe>
2021-04-25 06:31:35 -0500

    Lab06 2.- Info

--- index.js
+++ index.js
@@ -22,10 +22,13 @@ const lista = [
         name: "Mary Poppendick",
         number: "39-23-6423122",
     },
-]
+];
+let now= new Date();
 
+app.get('/info',(req,res) => {
+    res.send(`<h3>Phonebook has info for ${lista.length} people</h3><h3>`+now+"</h3>");
+});
 app.get('/api/persons',(req,res) => {
     res.send(lista);
 });
-
 app.listen(3001,()=>console.log('Abriendo en el Puerto 3001'))
