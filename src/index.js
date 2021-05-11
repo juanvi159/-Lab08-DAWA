f3f237eb0fc3754cb1792b22b4001044d2bc0124
VicenteObando <juan.obando.a@tecsup.edu.pe>
2021-05-10 21:28:59 -0500

    Conexion con Mongo

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 // const express = require('express');
 import express from 'express'
-const app = express()
+import lista from './utils/mockup'
+import MongoDB from './lib/mongo'
 
-const lista = [
-  {
-    id: 1,
-    name: 'Arto Hellias',
-    number: '040-123456'
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523'
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345'
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendick',
-    number: '39-23-6423122'
-  }
-]
+const app = express()
+const mongo = new MongoDB()
 const now = new Date()
 
 app.get('/info', (req, res) => {
@@ -64,11 +45,10 @@ app.post('/api/persons', (req, res) => {
   }
 
   const append = {
-    id: Math.round(Math.random() * 10000),
     name: req.body.name,
     number: req.body.number
   }
-  lista.push(append)
+  mongo.create('lista', append)
   res.send(append)
 })
 
---
+++ src/utils/mockup.js
@@ -0,0 +1,23 @@
+const lista = [
+    {
+      id: 1,
+      name: 'Juan Vicente',
+      number: '040-123456'
+    },
+    {
+      id: 2,
+      name: 'Vicente Alfredo',
+      number: '39-44-5323523'
+    },
+    {
+      id: 3,
+      name: 'Sand King',
+      number: '12-43-234345'
+    },
+    {
+      id: 4,
+      name: 'Mary Poppyns',
+      number: '39-23-6423122'
+    }
+  ]
+  export default lista;
